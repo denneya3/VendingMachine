d5c1b5d1491203765e0a4fb3831ba69bba6c8ae4
Alexander Denney <112534058+denneya3@users.noreply.github.com>
2023-02-20 23:03:32 -0800

    progress 2

--- SnackBar/Vendor.java
+++ SnackBar/Vendor.java
@@ -15,7 +15,7 @@ public class Vendor
     private int deposit;
     private int change;
     //make a private static double variable called totalSales that has an initial value of 0 and stores the amount of money made
-
+    private static double totalSales = 0;
 
     /**
      * Constructs a Vendor
@@ -25,17 +25,22 @@ public class Vendor
     public Vendor(int price, int stock)
     {
         //You need to complete this using this. notation
+        this.price = price;
+        this.stock = stock;
+        change = 0;
+        deposit = 0;
     }
 
     /**
      * Sets the quantity of items in stock.
-     * @param qty number of items to place in stock (int)
+     * @param stock number of items to place in stock (int)
      */
 
 
     public void setStock(int stock)
     {
         //You need to complete this using this. notation
+        this.stock = stock;
     }
 
     /**
@@ -45,6 +50,7 @@ public class Vendor
     public int getStock()
     {
         //complete this
+        return stock;
     }
 
     /**
@@ -55,6 +61,7 @@ public class Vendor
     public void addMoney(int d)
     {
         //You need to complete this using mutator
+        deposit += d;
     }
 
     /**
@@ -63,6 +70,7 @@ public class Vendor
      */
     public int getDeposit()
     {
+
         return deposit;
     }
 
@@ -78,6 +86,16 @@ public class Vendor
     public boolean makeSale()
     {
         //create the makesale method
+        if ((stock > 0) && (deposit >= price)){
+            stock--;
+            totalSales += price*0.01;
+            change = deposit-price;
+            deposit = 0;
+            return true;
+        }
+        change = deposit;
+        deposit = 0;
+        return false;
     }
 
     /**
@@ -98,9 +116,27 @@ public class Vendor
         /*
         note that the coin class has a .getQuarters(), a .getDimes() etc etc (use the coin class!)
         */
-        
+        int quarters, dimes, pennies, nickels;
 
-        String changeString="";
+        int leftOverChange = change;
+        Coins coins = new Coins(leftOverChange);
+        leftOverChange -= (coins.getQuarters()*25); // quarters
+        quarters = coins.getQuarters();
+
+        System.out.println(leftOverChange);
+
+        coins = new Coins(leftOverChange);
+        leftOverChange -= (coins.getDimes()*10); // dimes
+        dimes = coins.getDimes();
+
+        coins = new Coins(leftOverChange);
+        leftOverChange -= (coins.getNickles()*5); // nickels
+        nickels = coins.getNickles();
+
+        coins = new Coins(leftOverChange); //pennies
+        pennies = coins.getPennies();
+
+        String changeString=quarters+" quarter(s), "+dimes+" dime(s), "+nickels+" nickel(s), and "+pennies+" pennie(s).";
 
         return changeString;
     }
@@ -114,5 +150,8 @@ public class Vendor
     public static double getTotalSales()
     {
         //complete this
+        double t = totalSales;
+        totalSales = 0;
+        return t;
     }
 }
